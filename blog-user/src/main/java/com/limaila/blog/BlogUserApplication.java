5b0396cd674adaa506ca5b95854e9f5004aed60a
黄信程 <huangxincheng@xmles.cn>
2019-01-17 16:00:27 +0800

    init导入

---
+++ blog-user/src/main/java/com/limaila/blog/BlogUserApplication.java
@@ -0,0 +1,15 @@
+package com.limaila.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.multipart.MultipartFile;
+
+@SpringBootApplication
+public class BlogUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogUserApplication.class, args);
+	}
+
+}
+
