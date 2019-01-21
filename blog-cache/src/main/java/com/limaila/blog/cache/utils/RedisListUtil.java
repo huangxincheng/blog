f6160640d1390a0633156f33ea2bcb57c28adc8d
黄信程 <huangxincheng@xmles.cn>
2019-01-21 15:58:45 +0800

    增加RedisListUtil

--- blog-cache/src/main/java/com/limaila/blog/cache/utils/RedisListUtil.java
+++ blog-cache/src/main/java/com/limaila/blog/cache/utils/RedisListUtil.java
@@ -6,6 +6,7 @@ import org.springframework.data.redis.core.ListOperations;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -172,4 +173,82 @@ public class RedisListUtil {
     }
 
 
+    public static List<String> lrange(String key, long start, long end) {
+        try {
+            return listOperations.range(key, start, end);
+        } catch (Exception e) {
+            log.error("RedisListUtil lrange", e);
+            return null;
+        }
+    }
+
+    /**
+     * lrem [lrem key count value] :移除等于value的元素，
+     * 当count>0时，从表头开始查找，移除count个 移除等于value的元素；
+     * 当count=0时，从表头开始查找，移除所有等于value的；
+     * 当count<0时，从表尾开始查找，移除count个 移除等于value的元素。
+     * @param key key
+     * @param count count
+     * @param value value
+     * @return
+     */
+    public static Long lrem(String key, long count, String value) {
+        try {
+            return listOperations.remove(key, count, value);
+        } catch (Exception e) {
+            log.error("RedisListUtil lrem", e);
+            return null;
+        }
+    }
+
+
+    /**
+     * 根据下表获取列表中的值，下标是从0开始的
+     * @param key key
+     * @param index index
+     * @return
+     */
+    public static String lindex(String key, long index) {
+        try {
+            return listOperations.index(key, index);
+        } catch (Exception e) {
+            log.error("RedisListUtil lindex", e);
+            return null;
+        }
+    }
+
+    /**
+     * 将value的值设置到列表index位置
+     * 数组不能越界
+     * @param key key
+     * @param index index
+     * @param value value
+     * @return
+     */
+    public static boolean lset(String key, long index, String value) {
+        try {
+            listOperations.set(key, index, value);
+            return true;
+        } catch (Exception e) {
+            log.error("RedisListUtil lset", e);
+            return false;
+        }
+    }
+
+    /**
+     * 修剪现有列表，使其只保留指定的指定范围的元素，起始和停止都是基于0的索引
+     * @param key key
+     * @param start start
+     * @param end end
+     * @return
+     */
+    public static boolean ltrim(String key, long start, long end) {
+        try {
+            listOperations.trim(key, start, end);
+            return true;
+        } catch (Exception e) {
+            log.error("RedisListUtil ltrim", e);
+            return false;
+        }
+    }
 }
