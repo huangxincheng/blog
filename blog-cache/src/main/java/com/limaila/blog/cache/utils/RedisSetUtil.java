dc72d150d9c2b0295f4ef5bdc944581ca15dca48
黄信程 <huangxincheng@xmles.cn>
2019-01-21 20:01:39 +0800

    增加部分代码

--- blog-cache/src/main/java/com/limaila/blog/cache/utils/RedisSetUtil.java
+++ blog-cache/src/main/java/com/limaila/blog/cache/utils/RedisSetUtil.java
@@ -1,9 +1,16 @@
 package com.limaila.blog.cache.utils;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
 /**
  * Author: huangxincheng
  * <p>
  * <p>
  **/
+@Component
+@Slf4j
 public class RedisSetUtil extends RedisBaseUtil {
+
+
 }
