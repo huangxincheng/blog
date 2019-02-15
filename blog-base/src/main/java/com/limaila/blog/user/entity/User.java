70d3b0b0cf1fad9acd90bc99006a998effc4ff6b
黄信程 <huangxincheng@xmles.cn>
2019-02-15 14:15:29 +0800

    测试浅拷贝

--- blog-base/src/main/java/com/limaila/blog/user/entity/User.java
+++ blog-base/src/main/java/com/limaila/blog/user/entity/User.java
@@ -1,9 +1,19 @@
 package com.limaila.blog.user.entity;
 
+import lombok.*;
+import lombok.experimental.Accessors;
+import org.springframework.beans.BeanUtils;
+
 import java.io.Serializable;
 import java.util.Date;
 
-public class User implements Serializable {
+@Getter
+@Setter
+@ToString
+@Accessors(chain = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements Serializable,Cloneable {
     private Integer id;
 
     private String username;
@@ -20,125 +30,17 @@ public class User implements Serializable {
 
     private Boolean isCanUse;
 
-    private static final long serialVersionUID = 1L;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username == null ? null : username.trim();
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password == null ? null : password.trim();
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email == null ? null : email.trim();
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone == null ? null : phone.trim();
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    public Date getActiveTime() {
-        return activeTime;
-    }
-
-    public void setActiveTime(Date activeTime) {
-        this.activeTime = activeTime;
-    }
-
-    public Boolean getIsCanUse() {
-        return isCanUse;
-    }
-
-    public void setIsCanUse(Boolean isCanUse) {
-        this.isCanUse = isCanUse;
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(getClass().getSimpleName());
-        sb.append(" [");
-        sb.append("Hash = ").append(hashCode());
-        sb.append(", id=").append(id);
-        sb.append(", username=").append(username);
-        sb.append(", password=").append(password);
-        sb.append(", email=").append(email);
-        sb.append(", phone=").append(phone);
-        sb.append(", createTime=").append(createTime);
-        sb.append(", activeTime=").append(activeTime);
-        sb.append(", isCanUse=").append(isCanUse);
-        sb.append(", serialVersionUID=").append(serialVersionUID);
-        sb.append("]");
-        return sb.toString();
-    }
-
-    @Override
-    public boolean equals(Object that) {
-        if (this == that) {
-            return true;
-        }
-        if (that == null) {
-            return false;
-        }
-        if (getClass() != that.getClass()) {
-            return false;
-        }
-        User other = (User) that;
-        return (this.getId() == null ? other.getId() == null : this.getId().equals(other.getId()))
-            && (this.getUsername() == null ? other.getUsername() == null : this.getUsername().equals(other.getUsername()))
-            && (this.getPassword() == null ? other.getPassword() == null : this.getPassword().equals(other.getPassword()))
-            && (this.getEmail() == null ? other.getEmail() == null : this.getEmail().equals(other.getEmail()))
-            && (this.getPhone() == null ? other.getPhone() == null : this.getPhone().equals(other.getPhone()))
-            && (this.getCreateTime() == null ? other.getCreateTime() == null : this.getCreateTime().equals(other.getCreateTime()))
-            && (this.getActiveTime() == null ? other.getActiveTime() == null : this.getActiveTime().equals(other.getActiveTime()))
-            && (this.getIsCanUse() == null ? other.getIsCanUse() == null : this.getIsCanUse().equals(other.getIsCanUse()));
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((getId() == null) ? 0 : getId().hashCode());
-        result = prime * result + ((getUsername() == null) ? 0 : getUsername().hashCode());
-        result = prime * result + ((getPassword() == null) ? 0 : getPassword().hashCode());
-        result = prime * result + ((getEmail() == null) ? 0 : getEmail().hashCode());
-        result = prime * result + ((getPhone() == null) ? 0 : getPhone().hashCode());
-        result = prime * result + ((getCreateTime() == null) ? 0 : getCreateTime().hashCode());
-        result = prime * result + ((getActiveTime() == null) ? 0 : getActiveTime().hashCode());
-        result = prime * result + ((getIsCanUse() == null) ? 0 : getIsCanUse().hashCode());
-        return result;
+    public static void main(String[] args) throws CloneNotSupportedException {
+        User u1 = new User();
+        u1.setId(1).setUsername("111").setPassword("111");
+        User u2 = u1;
+        User u3 = (User)u1.clone();
+        System.out.println(u1.hashCode());
+        System.out.println(u2.hashCode());
+        System.out.println(u3.hashCode());
+        User u4 = new User();
+        BeanUtils.copyProperties(u1, u4);
+        System.out.println(u4.hashCode());
     }
 }
