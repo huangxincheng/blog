677ddf7a5a709d0d7ea3ecf7deb39e15272fe716
黄信程 <huangxincheng@xmles.cn>
2019-02-21 15:12:12 +0800

    x

--- blog-base/src/main/java/com/limaila/blog/user/entity/User.java
+++ blog-base/src/main/java/com/limaila/blog/user/entity/User.java
@@ -5,7 +5,8 @@ import lombok.experimental.Accessors;
 import org.springframework.beans.BeanUtils;
 
 import java.io.Serializable;
-import java.util.Date;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
@@ -32,15 +33,42 @@ public class User implements Serializable,Cloneable {
 
 
     public static void main(String[] args) throws CloneNotSupportedException {
-        User u1 = new User();
-        u1.setId(1).setUsername("111").setPassword("111");
-        User u2 = u1;
-        User u3 = (User)u1.clone();
-        System.out.println(u1.hashCode());
-        System.out.println(u2.hashCode());
-        System.out.println(u3.hashCode());
-        User u4 = new User();
-        BeanUtils.copyProperties(u1, u4);
-        System.out.println(u4.hashCode());
+//        User u1 = new User();
+//        u1.setId(1).setUsername("111").setPassword("111");
+//        User u2 = u1;
+//        User u3 = (User)u1.clone();
+//        System.out.println(u1.hashCode());
+//        System.out.println(u2.hashCode());
+//        System.out.println(u3.hashCode());
+//        User u4 = new User();
+//        BeanUtils.copyProperties(u1, u4);
+//        System.out.println(u4.hashCode());
+
+
+        User u1 = new User().setUsername("111").setId(111);
+        User u2 = new User().setUsername("222").setId(222);
+        User u3 = new User().setUsername("333").setId(333);
+
+        HashMap<Integer, User> map = new HashMap<>();
+        map.put(1, u2);
+        map.put(2, u1);
+        map.put(3, u3);
+        Map<Integer, User> integerUserMap = sortMap(map);
+        for (Map.Entry<Integer, User> integerUserEntry : integerUserMap.entrySet()) {
+            System.out.println(integerUserEntry);
+        }
+    }
+
+    public static Map<Integer, User> sortMap(HashMap<Integer, User> map) {
+        List<Map.Entry<Integer, User>> list = new ArrayList<>();
+        for (Map.Entry<Integer, User> entry : map.entrySet()) {
+            list.add(entry);
+        }
+        List<Map.Entry<Integer, User>> collect = list.stream().sorted((o1, o2) -> -o1.getValue().getId().compareTo(o2.getValue().getId())).collect(Collectors.toList());
+        Map<Integer, User> result = new LinkedHashMap<>();
+        for (Map.Entry<Integer, User> integerUserEntry : collect) {
+            result.put(integerUserEntry.getKey(), integerUserEntry.getValue());
+        }
+        return result;
     }
 }
